5630b1b5ac56a18c09591d02bd6ab06ebec957c9
Matthew Stromberg <mstromb1@bitbucket.org>
2016-06-15 17:28:27 -0400

    Removed unnecessary files, README now reflects instructions to run webapp

--- Angular/controllers/MainController.js
+++
@@ -1,9 +0,0 @@
-app.controller('MainController', ['$scope', '$http', function ($scope, $http) {
-    $scope.addPost = function() {
-        $http.get('http://localhost:3000/dowork', {
-            "msg": "hi"
-        }).success(function (data) {
-            console.log(data);
-        });
-    };
-}]);
